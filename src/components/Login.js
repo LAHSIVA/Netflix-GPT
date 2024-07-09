419b67c6bf482d3bde5ed2efdc8bd43d20622128
LAHSIVA <vishalvenkat2019@gmail.com>
2024-07-09 10:08:11 +0530

    SignIn Authentication Completed Got Access Token

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from 'react'
 import Header from './Header'
 import { checkValidData } from '../utils/Validate';
-import {createUserWithEmailAndPassword } from "firebase/auth";
+import {createUserWithEmailAndPassword,signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../utils/firebase";
 
 const Login = () => {
@@ -38,6 +38,17 @@ const Login = () => {
         }
         else{
             //SignInLogic
+            signInWithEmailAndPassword(auth, email.current.value,password.current.value)
+            .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            console.log(user);
+            })
+            .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            seterrorMessage(errorCode+"-"+errorMessage)
+            });
 
         }
 
