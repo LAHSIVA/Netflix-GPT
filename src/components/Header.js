636deddbbc7f38501ca92acf5b9181adf595f514
LAHSIVA <vishalvenkat2019@gmail.com>
2024-07-09 17:01:23 +0530

    Good fix: unsubscribed to the onAuthStateChanged callback when component unmounts to increase performance

--- src/components/Header.js
+++ src/components/Header.js
@@ -18,7 +18,7 @@ const Header = () => {
   };
 
   useEffect(()=>{
-    onAuthStateChanged(auth, (user) => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
         if (user) {
           const {uid,email,displayName,photoURL} = user;
           dispatch(
@@ -36,6 +36,9 @@ const Header = () => {
           
         }
       });
+
+      //unsubscribe when component unmounts
+      return()=> unsubscribe();
 },[]);
 
   return (
