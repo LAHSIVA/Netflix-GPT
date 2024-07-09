11f05c5372063a672b074729760056c939b3924b
LAHSIVA <vishalvenkat2019@gmail.com>
2024-07-09 15:29:48 +0530

    Successfully Created Signout Feature using SignOutApi

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,34 @@
 import React from 'react'
+import { signOut } from 'firebase/auth';
+import { auth } from '../utils/firebase';
+import {useNavigate } from 'react-router-dom';
 
 const Header = () => {
+  const navigate=useNavigate();
+  const handleSignOut=()=>{
+    signOut(auth).then(() => {
+      navigate("/");
+    }).catch((error) => {
+      navigate("/error");
+    });
+    
+  };
+
   return (
-    <div className='absolute px-8 py-2 bg-gradient-to-bl z-40'>
+    <div className='absolute w-screen px-8 py-2 bg-gradient-to-b from-slate-500 z-20 flex justify-between'>
         <img 
             className='w-40'
             src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
             alt="Logo"
         />
+        <div className="flex p-2">
+          <img 
+          className='w-12 h-12'
+          alt="usericon" 
+          src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png?20201013161117"
+          />
+          <button onClick={handleSignOut} className='font-bold text-red-700'>(Sign Out)</button>
+          </div>
     </div>
   )
 }
